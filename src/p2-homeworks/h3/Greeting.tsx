4a245aa1ee56b95a8d89391169a7ddaa729ffe4e
p.larionov <p.larionov@myspaceport.ru>
2022-08-25 21:26:00 +0500

    button add disabled

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -3,8 +3,8 @@ import s from './Greeting.module.css'
 
 type GreetingPropsType = {
     name: string // need to fix any
-    setNameCallback: (e:ChangeEvent<HTMLInputElement>)=>void // need to fix any
-    addUser: ()=>void // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
     error: string // need to fix any
     totalUsers: number // need to fix any
 }
@@ -13,14 +13,16 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error == '' ? s.no_error : s.error // need to fix with (?:)
+    const statusButton = name !== '' ? false : true;
+    const spanClassError = s.text_error;
 
     return (
         <div>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
+            <button onClick={addUser} disabled={statusButton}>add</button>
             <span>{totalUsers}</span>
+            <span className={spanClassError}>{error}</span>
         </div>
     )
 }
